f5e9e488d3a00fd2665a14956e0fe8e1bb40dd92
Kenan <kenan23@gmail.com>
2022-05-12 15:31:44 +0300

    Update admin support

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,24 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _  # for translation
+from . import models
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    ordering = ('id',)
+    list_display = ('email', 'name')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }), # for create new user   (password1 and password2 are required)
+    )
+
+
+admin.site.register(models.User, UserAdmin)
