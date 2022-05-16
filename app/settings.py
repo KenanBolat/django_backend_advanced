2c5ed57be6a971d44c12e39bfdda25db9689d870
Kenan <kenan23@gmail.com>
2022-05-16 16:35:48 +0300

    add manage user endpoint

--- app/settings.py
+++ app/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+#
 
 DATABASES = {
     'default': {
@@ -85,8 +85,8 @@ DATABASES = {
     }
 }
 
-
-
+#
+#
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
